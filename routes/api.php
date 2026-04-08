fac730ba7e51e7a87573ea81f33bab9c93de41b1
Celso Nery <celso.nery@gmail.com>
2026-04-08 00:41:08 -0300

    refactor: Remove '/' in Routes

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::get('/health', function () {
+    Route::get('health', function () {
         return response()->json(['message' => 'Api running']);
     });
 

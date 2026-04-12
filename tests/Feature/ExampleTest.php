7785b54b3f7672d2985c0d6fb56fa278dd62d6b5
Celso Nery <celso.nery@gmail.com>
2026-04-11 23:51:45 -0300

    test: Remove Example Tests

--- tests/Feature/ExampleTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Tests\Feature;
-
-it('test the application returns a successful', function () {
-    $response = $this->get('/api/v1/health');
-
-    $response->assertStatus(200);
-});

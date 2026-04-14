d236fcce2b79f853b39c1d4ff33355410401b338
Celso Nery <celso.nery@gmail.com>
2026-04-14 14:40:58 -0300

    chore: Add en app messages

---
+++ lang/en/app.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    // Authentication
+    'invalid_credentials' => 'Invalid credentials.',
+    'token_not_revoked' => 'User token not revoked.',
+    'user_logged_out' => 'User logged out successfully.',
+    'verification_link_invalid' => 'Invalid verification link.',
+
+    // E-mail validation
+    'email_already_verified' => 'E-mail already verified.',
+    'email_not_verified' => 'E-mail not verified.',
+    'email_verified_success' => 'E-mail verified successfully.',
+    'email_resend' => 'E-mail verification sent.',
+
+    // Reset password
+    'forgot_password' => 'You will received a link in your e-mail to recovery your password.',
+    'reset_password' => 'Password recovery successfully.',
+
+    // Create user
+    'user_created_error' => 'Error creating user.',
+    'user_created_success' => 'Usuer create successfully.',
+    
+];
--- lang/pt_BR/auth.php
+++ lang/pt_BR/auth.php
@@ -17,19 +17,4 @@ return [
     'password' => 'A senha informada está incorreta.',
     'throttle' => 'Muitas tentativas de login. Tente novamente em :seconds segundos.',
 
-    'invalid_credentials' => 'Credenciais inválidas',
-    'email_already_verified' => 'E-mail já verificado.',
-    'email_not_verified' => 'E-mail inda não verificado.',
-    'email_verified_success' => 'E-mail verificado com sucesso.',
-    'email_resend' => 'E-mail de verificação reenviado.',
-
-    'token_not_revoked' => 'User token not revoked.',
-    'user_logged_out' => 'User logged out successfully.',
-    'verification_link_invalid' => 'Link de verificação inválido.',
-
-    'forgot_password' => 'Você irá receber um e-mail com link para recuperação da senha.',
-    'reset_password' => 'Senha recuperada com sucesso.',
-
-    'user_created_error' => 'Erro criando usuário.',
-    'user_created_success' => 'Usuário criado com sucesso.',
 ];

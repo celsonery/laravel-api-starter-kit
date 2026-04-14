e4221c17e4f02cd3137ab1f47a8b69bcac0351d5
Celso Nery <celso.nery@gmail.com>
2026-04-14 14:23:04 -0300

    chore: Add pt-BR translate

---
+++ lang/pt_BR/auth.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Essas credenciais não foram encontradas em nossos registros.',
+    'password' => 'A senha informada está incorreta.',
+    'throttle' => 'Muitas tentativas de login. Tente novamente em :seconds segundos.',
+
+    'invalid_credentials' => 'Credenciais inválidas',
+    'email_already_verified' => 'E-mail já verificado.',
+    'email_not_verified' => 'E-mail inda não verificado.',
+    'email_verified_success' => 'E-mail verificado com sucesso.',
+    'email_resend' => 'E-mail de verificação reenviado.',
+
+    'token_not_revoked' => 'User token not revoked.',
+    'user_logged_out' => 'User logged out successfully.',
+    'verification_link_invalid' => 'Link de verificação inválido.',
+
+    'forgot_password' => 'Você irá receber um e-mail com link para recuperação da senha.',
+    'reset_password' => 'Senha recuperada com sucesso.',
+
+    'user_created_error' => 'Erro criando usuário.',
+    'user_created_success' => 'Usuário criado com sucesso.',
+];
